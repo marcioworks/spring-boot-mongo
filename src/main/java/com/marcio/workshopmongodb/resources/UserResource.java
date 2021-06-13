8668b2c35fcf0d063d104969a2f13e839483e34b
Marcio silva <marcioadsworks@gmail.com>
2021-06-13 11:12:36 -0300

    insert user service

--- src/main/java/com/marcio/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/marcio/workshopmongodb/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.marcio.workshopmongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,9 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.marcio.workshopmongodb.dto.UserDto;
 import com.marcio.workshopmongodb.entities.User;
@@ -37,5 +41,13 @@ public class UserResource {
 		User user = service.findById(id);
 			
 		return ResponseEntity.ok().body(new UserDto(user));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDto obj) {
+		User user = service.fromDto(obj);
+		service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/marcio/workshopmongodb/services/UserService.java
+++ src/main/java/com/marcio/workshopmongodb/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.marcio.workshopmongodb.dto.UserDto;
 import com.marcio.workshopmongodb.entities.User;
 import com.marcio.workshopmongodb.repositories.UserRespository;
 import com.marcio.workshopmongodb.services.exceptions.ObjectNotFoundException;
@@ -15,14 +16,21 @@ public class UserService {
 
 	@Autowired
 	private UserRespository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
-	
+
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Usuario não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDto(UserDto obj) {
+		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
 }
